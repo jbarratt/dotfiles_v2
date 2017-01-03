c54d654d27bc893c7826e35967576db3b6bfa531
Joshua Barratt <jbarratt@serialized.net>
2017-01-02 23:20:41 -0800

    added persistent home host

--- bin/dynamic_dns.sh
+++ bin/dynamic_dns.sh
@@ -5,7 +5,12 @@
 export AWS_PROFILE='serialized'
 export PATH="/usr/local/bin:/usr/bin/:$PATH"
 ZONE="serialized.net"
-HOSTNAME="initiative"
+case $HOSTNAME in
+    (gibson) declare -x HOSTNAME="warpdrive";;
+    (klee) declare -x  HOSTNAME="initiative";;
+    (*) declare -x HOSTNAME="initiative";;
+esac
+echo "$HOSTNAME"
 REMOTE_IP=$(curl -s ipecho.net/plain)
 CURRENT_VALUE=$(dig +short $HOSTNAME.$ZONE)
 
